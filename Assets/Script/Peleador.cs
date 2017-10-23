b6bae338f1817867b95c4bec8e1ec39e58c34c2c
cosimita15 <cosimagrego_15@hotmail.com>
2017-10-23 16:16:08 -0300

    añadidos triggers de morir y curarse

--- Assets/Script/Peleador.cs
+++ Assets/Script/Peleador.cs
@@ -74,6 +74,8 @@ public class Peleador : MonoBehaviour {
 		sigueVivo = false;
 		if (vida > 0) {
 			sigueVivo = true;
+		} else {
+			animator.SetTrigger ("Morir");
 		}
 	}
 
@@ -123,49 +125,24 @@ public class Peleador : MonoBehaviour {
 			CambiarMana (-accion.costoMana);
 			if (accion.objetivoEsElMismo) { 
 				objetivo = transform;
+				if(accion.nombre== "Curar"){
+					animator.SetTrigger ("Curar");
+				}
+			
 			}
 
 
 
-			//if (accion.estatico) {
-			//anim.SetTrigger (accion.animacionTrigger);
+			if(accion.nombre== "Golpear")
+				animator.SetTrigger ("Attack");	
 
-				if(accion.nombre== "Golpear")
-					animator.SetTrigger ("Attack");	
+			objetivo.SendMessage (accion.mensaje, accion.argumento);
+			yield return new WaitForSeconds (1/2);
 
-				objetivo.SendMessage (accion.mensaje, accion.argumento);
-				yield return new WaitForSeconds (1/2);
 
-				/*
-			} else {
-				Vector3 PosInicial = transform.position;
-
-				transform.LookAt (objetivo.transform.position);
-				//nv.SetDestination (objetivo.position);
-				//anim.SetFloat("Speed", 1);
-
-				while (Vector3.Distance (transform.position, objetivo.position) > 2)
-					yield return null;
-				//nv.speed = 0;
-				//anim.SetFloat("Speed", 0);
-
-				yield return new WaitForSeconds (0.5f);
-				//anim.SetTrigger (accion.animacionTrigger);
-				yield return new WaitForSeconds (0.1f);
-				objetivo.SendMessage (accion.mensaje, accion.argumento);
-				yield return new WaitForSeconds (1);
-
-				transform.LookAt (PosInicial);
-				//nv.SetDestination (PosInicial);
-				//nv.speed = 3.5f;
-				//anim.SetFloat("Speed", 1);
-
-				while (Vector3.Distance (transform.position, objetivo.position) > 0.1f)
-					yield return null;
-				//anim.SetFloat("Speed", 0);
-
+			if (accion.nombre == "Especial") {
+				animator.SetTrigger ("Morder");
 			}
-			*/
 			
 		}
 	}
